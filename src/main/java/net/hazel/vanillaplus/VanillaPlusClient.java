304e21118ee271e3874ca42f1c0aec5c0a4d4063
TheMrRedstone <87324700+TheMrRedstone@users.noreply.github.com>
2023-09-16 18:21:56 -0400

    Initial commit

---
+++ src/main/java/net/hazel/vanillaplus/VanillaPlusClient.java
@@ -0,0 +1,10 @@
+package net.hazel.vanillaplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hazel/vanillaplus/VanillaPlusDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hazel.vanillaplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaPlusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
